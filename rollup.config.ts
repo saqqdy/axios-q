7da614677698d34a89bd5014d9827f75d5802c01
saqqdy <saqqdy_wu@kingdee.com>
2021-09-18 09:39:25 +0800

    优化代码

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,7 +6,7 @@ import typescript from 'rollup-plugin-typescript2'
 import { visualizer } from 'rollup-plugin-visualizer'
 import pkg from './package.json'
 
-const config = require('./config')
+import config from './config.json'
 
 // const production = !process.env.ROLLUP_WATCH
 
